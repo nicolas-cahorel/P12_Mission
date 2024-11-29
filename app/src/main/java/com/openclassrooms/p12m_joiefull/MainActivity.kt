291dc63fee257cbf8e5e07fbc6d0cb380b656964
Nicolas Cahorel <nicolas.cahorel@gmail.com>
2024-11-29 10:05:18 +0100

    first commit

---
+++ app/src/main/java/com/openclassrooms/p12m_joiefull/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.openclassrooms.p12m_joiefull
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.openclassrooms.p12m_joiefull.ui.theme.P12M_joiefullTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            P12M_joiefullTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    P12M_joiefullTheme {
+        Greeting("Android")
+    }
+}
